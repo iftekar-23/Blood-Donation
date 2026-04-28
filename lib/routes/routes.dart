495d7ef965ecff373e4c305590213409d74a5993
iftekar-23 <aiftekar798@gmail.com>
2026-04-28 14:57:35 +0600

    complete splash screen

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../splash/splash_screen.dart';
+
 enum AppRoutes{
   splash,
   main,
---
+++ lib/splash/splash_screen.dart
@@ -0,0 +1,79 @@
+import 'package:blood_donation/routes/routes.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin{
+
+  late AnimationController _controller;
+  late Animation<Offset> _logoAnimation;
+  late Animation<Offset> _textController;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: const Duration( seconds: 2));
+    _logoAnimation = Tween<Offset>(
+      begin: const Offset(0, -1),
+      end: const Offset(0, 0)
+    ).animate(
+      CurvedAnimation(parent: _controller, curve: Curves.easeInOut)
+    );
+
+    _textController = Tween<Offset>(
+        begin: const Offset(-1, 0),
+        end: const Offset(0, 0)
+    ).animate(
+        CurvedAnimation(parent: _controller, curve: Curves.easeInOut)
+    );
+
+    _controller.forward();
+
+    Future.delayed( const Duration(seconds: 3), () {
+      context.goNamed(AppRoutes.main.name);
+    },);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF680c07),
+      body: Center(
+        child: Column(
+          children: [
+            SlideTransition(
+              position: _logoAnimation,
+              child: Image.asset("assets/logo.png",
+                width: 150,
+              ),
+            ),
+            SizedBox(height: 20,),
+
+            SlideTransition(
+              position: _textController,
+              child: Text("Blood donation App")
+            ),
+
+            SlideTransition(
+                position: _textController,
+                child: Text("Donate & Save Live!")
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
