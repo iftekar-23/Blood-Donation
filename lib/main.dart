c99184948553cd7ea398ef09e35b79b5f2f78f8b
iftekar-23 <aiftekar798@gmail.com>
2026-04-28 14:25:31 +0600

    added splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: const MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp.router(
+      routerConfig: ,
       debugShowCheckedModeBanner: false,
       title: 'Blood Donation',
       theme: ThemeData(
---
+++ lib/routes/routes.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+enum AppRoutes{
+  splash,
+  main,
+  signIn,
+  register,
+  account,
+  bloodGroupSelected,
+  emailedUsers,
+  notifications
+}
+
+GoRouter goRouter (GoRouterRef ret){
+  return GoRouter(routes: [
+    GoRoute(
+        path: '/splash',
+      name: AppRoutes.splash.name,
+      builder: (ctx, state) => const SplashScreen()
+    )
+  ]);
+}
